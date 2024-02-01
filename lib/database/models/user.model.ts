90da8b1311ff3ac8e2886ea05b8e73dc23f14ad7
yazmonto7 <158222247+yazmonto7@users.noreply.github.com>
2024-02-01 09:33:03 -0500

    Update user.model.ts

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -13,9 +13,9 @@ const UserSchema = new Schema({
   clerkId: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
-  firstName: { type: String, required: true, unique: true },
-  lastName: { type: String, required: true, unique: true },
-  photo: { type: String, required: true, unique: true },
+  firstName: { type: String },
+  lastName: { type: String },
+  photo: { type: String },
 });
 
 const User = models.User || model("User", UserSchema);
